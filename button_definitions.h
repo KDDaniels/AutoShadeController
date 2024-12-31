2360f63fb57ccfa57d33ffa71bbeec3018a51c29
AgehWewt <kendall.daniels.dev@gmail.com>
2024-12-30 19:11:29 -0600

    added remote button definitions

---
+++ button_definitions.h
@@ -0,0 +1,27 @@
+#define POWER       0x45
+
+#define VOLUP       0x46
+#define VOLDOWN     0x15
+
+#define FUNC        0x47
+
+#define SEEK_LEFT   0x44
+#define PLAY        0x40
+#define SEEK_RIGHT  0x43
+
+#define DOWN        0x7
+#define UP          0x9
+
+#define EQ          0x19
+#define REPT        0xD
+
+#define ZERO        0x16
+#define ONE         0xC
+#define TWO         0x18
+#define THREE       0x5E
+#define FOUR        0x8
+#define FIVE        0x1C
+#define SIX         0x5A
+#define SEVEN       0x42
+#define EIGHT       0x52
+#define NINE        0x4A
