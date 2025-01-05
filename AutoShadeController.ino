078faeff3fb0afae4b57f4141b6f9769d313f49b
AgehWewt <kendall.daniels.dev@gmail.com>
2025-01-05 17:26:21 -0600

    rewrote logic and fixed some errors

--- AutoShadeController.ino
+++ AutoShadeController.ino
@@ -17,26 +17,6 @@
 * along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-/*
-thoughts
-
-POWER will toggle automatic opening on or off for the selected shade, I think 0 will select both, 1 will select
-motor 1, and 2 will select motor 2
-
-UP and DOWN are self explanatory (up makes it go up, down makes it go down)
-
-FUNC will act as a calibrate, shade starts all the way open and steps = 0, so 0 is all the way open
-shade moves down and steps go up, pressing FUNC at the bottom will let the controller know
-it's at the bottom and can save max steps for that shade
-this'll need to be split between two shades of course, but that shouldn't be too hard
-save the max values in EEPROM, probably the first two addresses that are available
-
-PLAY/PAUSE will save target steps, so you can set where you want the shade to open to instead
-of fully opening
-gotta figure out circle buffer or whatever so it can save the current steps when it reaches target or max
-will make it pretty much last forever hopefully
-*/
-
 #if defined(__AVR_ATtiny85__)
 #define IR_RECEIVE_PIN  PB4
 #define LIGHT_SENSE PB3
@@ -79,31 +59,28 @@ bool calibrating = false;
 bool motorOneOpen = true;
 bool motorTwoOpen = true;
 
+bool motorOneOpening = false;
+bool motorTwoOpening = false;
+
+bool motorOneClosing = false;
+bool motorTwoClosing = false;
+
 bool opening = false;
 bool closing = false;
-
 
 bool flashState = false;
 unsigned long flashTarget = 0;
 unsigned long flashDelay = 250;
 unsigned long flashLongDelay = 10000;
 
+bool stepperStopped = true;
+
 
 // ===== Light stuff ========
-const uint8_t NUM_OF_READINGS = 3;
-uint16_t readings[NUM_OF_READINGS]; // using an array of readings to get the average light level (more accurate)
-
-uint16_t totalVal = 0;
 uint16_t averageVal = 0;
-uint8_t senseLoopNum = 0;
-uint8_t readIndex = 0;
-
-unsigned long avgDelay = 10; // delay between readings
-unsigned long readDelay = 1000; // 10 minutes in milliseconds (600,000ms)
+const uint16_t THRESHOLD = 20;
+unsigned long readDelay = 5000; // 10 minutes in milliseconds (600,000ms)
 unsigned long targetMillisSensor = 0;
-unsigned long targetMillisAvg = 0;
-
-bool sensing = false;
 
 
 void setup()
@@ -111,16 +88,16 @@ void setup()
   IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);
   pinMode(LIGHT_SENSE, INPUT);
 
-  // Initializes the readings array with 0
-  for (uint8_t x = 0; x < NUM_OF_READINGS; x++) { readings[x] = 0; }
-
   // Load max steps
   if (EEPROM.read(0x01) != 0xFF) {
     EEPROM.get(0x00, motorOneMax);
     EEPROM.get(0x02, motorTwoMax);
     EEPROM.get(0x04, motorOneTarget);
     EEPROM.get(0x06, motorTwoTarget);
+    
   }
+
+  stepper.setDelay(3);
 
   stepper.stop();
 }
@@ -130,113 +107,152 @@ void loop()
   currentMillis = millis();
   checkIR();
 
+
   if (closing == true)
   {
-    if (activeMotor == 0)
+    switch (activeMotor)
     {
-      if ((motorOneSteps < motorOneMax) && (motorTwoSteps < motorTwoMax))
-      {
-        if (stepper.step(CW)) { motorOneSteps++; motorTwoSteps++; }
-      }
-      else if (motorOneSteps < motorOneMax) 
-      {
-        if (stepper.step(MOTOR_1, CW)) { motorOneSteps++; }
-      }
-      else if (motorTwoSteps < motorTwoMax) 
-      {
-        if (stepper.step(MOTOR_2, CW)) { motorTwoSteps++; }
-      }
-    }
-    else
-    {
-      if ((activeMotor == 1) && (motorOneSteps < motorOneMax))
-      {
-        if (stepper.step(MOTOR_1, CW)) { motorOneSteps++; }
-      }
-      else if ((activeMotor == 2) && (motorTwoSteps < motorTwoMax))
-      {
-        if (stepper.step(MOTOR_2, CW)) { motorTwoSteps++; }
-      }
+      case 0:
+
+        if ((motorOneSteps < motorOneMax) && (motorTwoSteps < motorTwoMax))
+        {
+          if (stepper.step(CW)) { motorOneSteps++; motorTwoSteps++; }
+        }
+        else if (motorOneSteps < motorOneMax)
+        {
+          if (stepper.step(MOTOR_1, CW)) { motorOneSteps++; }
+        }
+        else if (motorTwoSteps < motorTwoMax)
+        {
+          if (stepper.step(MOTOR_2, CW)) { motorTwoSteps++; }
+        }
+
+        break;
+
+      case 1:
+
+        if (motorOneSteps < motorOneMax)
+        {
+          if (stepper.step(MOTOR_1, CW)) { motorOneSteps++; }
+        }
+
+        break;
+
+      case 2:
+
+        if (motorTwoSteps < motorTwoMax)
+        {
+          if (stepper.step(MOTOR_2, CW)) { motorTwoSteps++; }
+        }
+
+        break;
     }
 
-    if ((motorOneSteps == motorOneMax) && (motorTwoSteps == motorTwoMax))
+    if (motorOneSteps >= motorOneMax)
     {
       motorOneOpen = false;
+    }
+
+    if (motorTwoSteps >= motorTwoMax)
+    {
       motorTwoOpen = false;
+    }
+
+    if (motorOneOpen == false && motorTwoOpen == false)
+    {
       closing = false;
       stepper.stop();
+      stepperStopped = true;
     }
   }
   else if (opening == true)
   {
-    if (activeMotor == 0)
+    switch (activeMotor)
     {
-      if ((motorOneSteps > motorOneTarget) && (motorTwoSteps > motorTwoTarget))
-      {
-        if (stepper.step(CCW)) { motorOneSteps--; motorTwoSteps--; }
-      }
-      else if (motorOneSteps > motorOneTarget) 
-      {
-        if (stepper.step(MOTOR_1, CCW)) { motorOneSteps--; }
-      }
-      else if (motorTwoSteps > motorTwoTarget) 
-      {
+      case 0:
+        if ((motorOneSteps > motorOneTarget) && (motorTwoSteps > motorTwoTarget))
+        {
+          if (stepper.step(CCW)) { motorOneSteps--; motorTwoSteps--; }
+        }
+        else if (motorOneSteps > motorOneTarget)
+        {
+          if (stepper.step(MOTOR_1, CCW)) { motorOneSteps--; }
+        }
+        else if (motorTwoSteps > motorTwoTarget)
+        {
+          if (stepper.step(MOTOR_2, CCW)) { motorTwoSteps--; }
+        }
+        break;
+
+      case 1:
+        if (motorOneSteps > motorOneTarget)
+        {
+          if (stepper.step(MOTOR_1, CCW)) { motorOneSteps--; }
+        }
+        break;
+
+      case 2:
         if (stepper.step(MOTOR_2, CCW)) { motorTwoSteps--; }
-      }
-    }
-    else
-    {
-      if ((activeMotor == 1) && (motorOneSteps > motorOneTarget))
-      {
-        if (stepper.step(MOTOR_1, CCW)) { motorOneSteps--; }
-      }
-      else if ((activeMotor == 2) && (motorTwoSteps > motorTwoTarget))
-      {
-        if (stepper.step(MOTOR_2, CCW)) { motorTwoSteps--; }
-      }
+        break;
     }
 
-    if ((motorOneSteps == motorOneTarget) && (motorTwoSteps == motorTwoTarget))
+    if (motorOneSteps <= motorOneTarget)
     {
       motorOneOpen = true;
+    }
+
+    if (motorTwoSteps <= motorTwoTarget)
+    {
       motorTwoOpen = true;
+    }
+
+    if (motorOneOpen == true && motorTwoOpen == true)
+    {
       opening = false;
       stepper.stop();
+      stepperStopped = true;
     }
   }
 
   // checks if light sensing should occur this loop
-  if (currentMillis >= targetMillisSensor)
+  if (isAuto == true)
   {
-    averageVal = (analogRead(LIGHT_SENSE) + analogRead(LIGHT_SENSE)) / 2;
-    targetMillisSensor += readDelay;
-  }
 
-  // move all motor checks into an if isAuto statement
-  if ((isAuto == true) && (averageVal >= 20))
-  {
-    if (motorOneOpen == true && motorTwoOpen == true)
+    if (currentMillis >= targetMillisSensor)
     {
-      opening = false;
+      averageVal = 0;
+      for (uint8_t x = 0; x < 10; x++)
+      {
+        averageVal += analogRead(LIGHT_SENSE); 
+      }
+      averageVal /= 10;
+      targetMillisSensor += readDelay;
+    }
+
+    if (averageVal >= THRESHOLD + 5 && opening == false)
+    {
+      if ((motorOneSteps > motorOneTarget || motorTwoSteps > motorTwoTarget) || (motorOneOpen == false || motorTwoOpen == false))
+      {
+        opening = true;
+      }
+    }
+    else if (averageVal < THRESHOLD && closing == false)
+    {
+      if ((motorOneSteps < motorOneMax) || (motorTwoSteps < motorTwoMax) || (motorOneOpen == true || motorTwoOpen == true))
+      {
+        closing = true;
+      }
     }
     else
     {
-      opening = true;
-    }
-  } 
-  else if ((isAuto == true) && (averageVal < 20)) 
-  {
-    if (motorOneOpen == false && motorTwoOpen == false)
-    {
-      closing = false;
-    }
-    else
-    {
-      closing = true;
+      if (stepperStopped == false)
+      {
+        stepper.stop();
+        stepperStopped = true;
+      }
     }
   }
-
-  if ((isAuto == false) && (opening == false) && (closing == false))
+  else if (opening == false && closing == false)
   {
     if (currentMillis >= flashTarget)
     {
@@ -250,10 +266,15 @@ void loop()
   // only calibrate one motor at a time
   if (calibrating == true)
   {
-    if (activeMotor == 1) {
-      if (stepper.step(MOTOR_1, CW)) { motorOneSteps++; }
-    } else if (activeMotor == 2) {
-      if (stepper.step(MOTOR_2, CW)) { motorTwoSteps++; }
+    switch (activeMotor) {
+      
+      case 1:
+        if (stepper.step(MOTOR_1, CW)) { motorOneSteps++; }
+        break;
+
+      case 2:
+        if (stepper.step(MOTOR_2, CW)) { motorTwoSteps++; }
+        break;
     }
   }
 }
@@ -283,39 +304,62 @@ void checkIR()
         switch (IrReceiver.decodedIRData.command) {
           case POWER:
             isAuto = !isAuto;
+            activeMotor = 0;
             stepper.stop();
             break;
 
           case FUNC: // Calibrate
-            calibrating = !calibrating;
-            if (calibrating == false) {
-              // save max steps into EEPROM when calibrating switches back to false
-              if (activeMotor == 1) 
-              { EEPROM.put(0x00, motorOneSteps); motorOneMax = motorOneSteps; }
-              else if (activeMotor == 2) 
-              { EEPROM.put(0x02, motorTwoSteps); motorTwoMax = motorTwoSteps; }
-              stepper.stop();
+            if (activeMotor != 0)
+            {
+              calibrating = !calibrating;
+              isAuto = false;
+              if (calibrating == false) {
+
+                if (activeMotor == 1) 
+                {
+                  // EEPROM.put(0x00, motorOneSteps);
+                  motorOneMax = motorOneSteps;
+                  motorOneOpen = false;
+                }
+                else if (activeMotor == 2) 
+                {
+                  // EEPROM.put(0x02, motorTwoSteps);
+                  motorTwoMax = motorTwoSteps;
+                  motorTwoOpen = false;
+                }
+                stepper.stop();
+              }
             }
             break;
 
           case PLAY: // Set target shade position
             if (activeMotor == 1)
-            { EEPROM.put(0x04, motorOneSteps); motorOneTarget = motorOneSteps; }
+            {
+              // EEPROM.put(0x04, motorOneSteps);
+              motorOneTarget = motorOneSteps;
+            }
             else if (activeMotor == 2)
-            { EEPROM.put(0x06, motorTwoSteps); motorTwoTarget = motorOneSteps; }
+            {
+              // EEPROM.put(0x06, motorTwoSteps);
+              motorTwoTarget = motorTwoSteps;
+            }
+            opening = false;
+            closing = false;
             stepper.stop();
             break;
 
           case UP:
             isAuto = false;
-            opening = true;
+            opening = !opening;
             closing = false;
+            stepper.stop();
             break;
 
           case DOWN:
             isAuto = false;
             opening = false;
-            closing = true;
+            closing = !closing;
+            stepper.stop();
             break;
 
           case ZERO:
@@ -326,15 +370,15 @@ void checkIR()
             break;
 
           case ONE:
+            activeMotor = 1;
             opening = false;
             closing = false;
-            activeMotor = 1;
             break;
 
           case TWO:
+            activeMotor = 2;
             opening = false;
             closing = false;
-            activeMotor = 2;
             break;
 
           default:
